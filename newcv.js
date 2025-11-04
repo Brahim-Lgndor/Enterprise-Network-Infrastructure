e7a353a2e49e0ab20a56362d8937a377a52457e6
Brahim-Lgndor <brahimprogontop@gmail.com>
2025-11-04 20:10:12 +0100

    Add files via upload

---
+++ newcv.js
@@ -0,0 +1,18 @@
+let darkmode = localStorage.getItem("darkmode");
+
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+}   
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+}
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode()
+})
